045745f712e0df7165db4d18a982d59fd76ea35c
Michael Grudkowski <grudkowm@Michaels-MBP.fios-router.home>
2023-07-22 12:15:41 -0400

    initial commit

---
+++ omz/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+alias cd..=cd ..
+alias cd...=cd ../../
+alias cd....=cd ../../../
+
+
+alias s="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias type='type -f'
+alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+
+alias k="kubectl"
